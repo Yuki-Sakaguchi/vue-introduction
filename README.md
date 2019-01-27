b2942437f166afd972adf2c287aa926e0c69b4da
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2019-01-27 23:19:41 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vue.js入門
+[Vue.js入門](https://www.amazon.co.jp/dp/4297100916/ref=asc_df_42971009162574492/?tag=jpgo-22&creative=9303&creativeASIN=4297100916&linkCode=df0&hvadid=295678107984&hvpos=1o1&hvnetw=g&hvrand=12801326749273451696&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1009308&hvtargid=pla-529052961492&th=1&psc=1)に取り組む
+
+# DEMO
+https://yuki-sakaguchi.github.io/vue-introduction/part01/
---
+++ part01/app.js
@@ -0,0 +1,106 @@
+var items = [
+  {
+    name: '鉛筆',
+    price: 300,
+    quantity: 0,
+  },
+  {
+    name: 'ノート',
+    price: 400,
+    quantity: 0,
+  },
+  {
+    name: '消しゴム',
+    price: 500,
+    quantity: 0,
+  },
+]
+
+var vm = new Vue({
+  el: '#app',
+
+  // データ
+  data: {
+    items: items,
+  },
+
+  // メソッド
+  methods: {
+    doBuy: function (e) {
+      alert(this.$options.filters.numberWithDelimiter(this.totalPriceWithTax) + '円のお買い上げ！\nありがとうございます！')
+      this.items.forEach(function (item) {
+        item.quantity = 0
+      })
+    },
+  },
+
+  // 文字列にフォーマットをかける関数
+  filters: {
+    numberWithDelimiter: function (value) {
+      return value ? value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') : '0'
+    },
+  },
+
+  // dataそのものに処理を加えたものをプロパティとして扱える関数
+  computed: {
+    totalPrice: function () {
+      return this.items.reduce(function (sum, item) {
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    totalPriceWithTax: function () {
+      return Math.floor(this.totalPrice * 1.08)
+    },
+    canBuy: function () {
+      return this.totalPrice >= 1000
+    },
+    errorMessageClass: function () {
+      return {
+        error: !this.canBuy
+      }
+    },
+    errorMessageStyle: function () {
+      return {
+        border: this.canBuy ? '1px solid black' : '1px solid red',
+        color: this.canBuy ? '' : 'red',
+        padding: '10px 20px',
+      }
+    },
+  },
+
+  beforeCreate: function () {
+    console.log('beforeCreate; 「インスタンスが生成され、データが初期化される前」に動く処理')
+  },
+  /**
+   * DOMにマウントされる前なのでDOMの操作はできない
+   * WebAPIと通信したりするのはこのタイミング
+   */
+  created: function () {
+    console.log('created: 「インスタンスが生成され、データが初期化された後」に動く処理')
+  },
+  beforeMount: function () {
+    console.log('beforeMount: 「インスタンスがDOM要素にマウントされる前」に動く処理')
+  },
+  /**
+   * DOMにマウントされた後なのでDOMにアクセスできるのでイベントリスナーの登録はここでやる
+   */
+  mounted: function () {
+    console.log('mounted: 「インスタンスがDOM要素にマウントされた後」に動く処理')
+  },
+  beforeUpdate: function () {
+    console.log('beforeUpdate: 「データが変更され、DOMに適用される前」に動く処理')
+  },
+  updated: function () {
+    console.log('updated: 「データが変更され、DOMに適用された後」に動く処理')
+  },
+  /**
+   * イベントリスナーの破棄、タイマーの解除などの後始末はここでやる
+   * 
+   */
+  beforeDestroy: function () {
+    console.log('beforeDestroy: 「Vueインスタンスが破棄される前」に動く処理')
+  },
+  destroyed: function () {
+    console.log('destroyed: 「Vueインスタンスが破棄された後」に動く処理')
+  },
+})
