73c84c9973121b4bc52a0519b8ed9309fc9b6ae9
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2019-01-30 22:15:57 +0900

    [mod] コメント修正

---
+++ part04/app.js
@@ -0,0 +1,52 @@
+/**
+ * Vueコンポーネント
+ *    props と event
+ *      親コンポーネントから子コンポーネントに値を渡すにはprops
+ *      子コンポーネントから親コンポーネントに値を渡すにはevent
+ **/
+
+// 子コンポーネントから親コンポーネントに値を渡すにはevent
+var counterButton = Vue.extend({
+  template: '<span>{{ count }}個<button v-on:click="addToCart">追加</button></span>',
+  data () {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    addToCart () {
+      this.count++
+      this.$emit('increment') // incrementカスタムイベントを発火
+    }
+  }
+})
+
+// 親コンポーネントから子コンポーネントに値を渡すにはprops
+Vue.component('fruits-item-name', {
+  template: '<div>{{ fruitsItem.name }}</div>',
+  props: {
+    fruitsItem: { // テンプレートではケバブケース、HTMLはハイフンつなぎ
+      type: Object, // 型
+      required: true, // 必須
+    }
+  }
+})
+
+new Vue({
+  el: '#fruits-component',
+  components: {
+    'counter-button': counterButton,
+  },
+  data: {
+    total: 0,
+    fruitsItems: [
+      { name: 'いちご' },
+      { name: 'ぶどう' },
+    ]
+  },
+  methods: {
+    incrementCartStates () {
+      this.total += 1
+    }
+  }
+})
