9c8f7a2fcd131862f7488b125bdd30a55aafc6db
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2019-02-03 19:44:39 +0900

    [mod]

--- part06/app.js
+++ part06/app.js
@@ -1,3 +1,31 @@
 /**
- * Vueコンポーネント
+ * 実践的なコンポーネント
+ *    - slot
  */
+
+// slotタグを使えばデフォルトの要素を用意しつつ
+// コンポーネントを使う際にごそっと入れ替えることができる
+// 使い側は[slot="name"] = <slot name="name">という感じで使う
+var headerTemplate = `
+  <div style="color: gray;">
+    <slot name="header">No title.</slot>
+  </div>
+`
+
+var contentTemplate = `
+  <div style="color: gray;">
+    <slot name="content">No contents.</slot>
+  </div>
+`
+
+Vue.component('page-header', {
+  template: headerTemplate,
+})
+
+Vue.component('page-content', {
+  template: contentTemplate,
+})
+
+new Vue({
+  el: '#fruits-list',
+})
